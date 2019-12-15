e384c32ff4a10206b655a1946083e5d507f95ce2
Ricardo Saffi Marques <rsaffi@rsaffi.com>
2019-12-15 18:02:34 +0100

    First library

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rsaffi/go/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
